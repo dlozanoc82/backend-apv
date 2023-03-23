8670ab05b07e8d67b646be40490651cc4b2ef75c
Daniel <dlozanoc862@gmail.com>
2023-03-23 11:03:51 -0500

    :sparkles: Se inicia el servidor de Express

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 import express from "express";
+
+const app = express();
+
+app.use('/', ( req, res ) => {
+    res.send("Hola Daniel");
+})
+
+app.listen(4000, ()=>{
+    console.log('Servidor funcionando en el puerto 4000')
+})
