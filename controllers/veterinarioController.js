679392d67b69fd202babcb41e45dede3527ce21e
Daniel <dlozanoc862@gmail.com>
2023-03-23 13:14:25 -0500

    :sparkles: Se agregan las rutas y los controladores para ruta veterinario

---
+++ controllers/veterinarioController.js
@@ -0,0 +1,13 @@
+
+const registrar = ( req, res )=>{
+    res.send('Desde API/VETERINARIOS');
+};
+
+const perfil =( req, res )=>{
+    res.send('Desde API/VETERINARIOS/perfil');
+}
+
+export {
+    registrar,
+    perfil,
+}
