bac1e6305450bdfeac0879a99eb0dcfe20f6c808
Daniel <dlozanoc862@gmail.com>
2023-03-23 20:50:53 -0500

    :bug: Comprobar si el usuario ha confirmado su cuenta

--- controllers/veterinarioController.js
+++ controllers/veterinarioController.js
@@ -61,7 +61,10 @@ const autenticar = async( req, res ) => {
     }
 
     //Comprobar si el usuario esta confirmado
-    
+    if (!usuario.confirmado) {
+        const error = new Error('Tu cuenta no ha sido confirmada');
+        return res.status(403).json({msg: error.message});
+    }
 
 }
 
