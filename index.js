86f54718f94463bc5a155e81d11101c33a29df6b
Daniel <dlozanoc862@gmail.com>
2023-04-01 10:55:30 -0500

    :closed_lock_with_key: Se optimizan los endpoints usando variables de entorno

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ dotenv.config();
 conectarDB();
 
 //Cors
-const dominiosPermitidos = ['http://localhost:5173'];
+const dominiosPermitidos = [process.env.FRONTEND_URL];
 
 const corsOptions = {
     origin: function (origin, callback) {
